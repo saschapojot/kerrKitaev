7d1ada901921126467468d1a4b3db2462ec9c8ea
Xi Liu <1992xi.liu@gmail.com>
2020-09-15 21:23:32 +0800

    This is the initialization of this repo

---
+++ consts.hpp
@@ -0,0 +1,14 @@
+//
+// Created by tusa on 15/9/20.
+//
+
+#ifndef KERRKITAEV_CONSTS_HPP
+#define KERRKITAEV_CONSTS_HPP
+class CONSTS{
+
+    int N=500;
+    double Nf=(double )N;
+
+
+};
+#endif //KERRKITAEV_CONSTS_HPP
